e3aa4609e4128af2e8757b27795a8527fb0cd5fa
isa <54730392+isabellozano@users.noreply.github.com>
2019-10-16 23:38:35 -0700

    passwordhomework

---
+++ PasswordGenerator.js
@@ -0,0 +1,9 @@
+// for your button / prompt - have a placeholder text asking how long they want there password and it must be a number stating between 8 - 128 characters
+
+// then ask if they want any special characters (this should be a conditional/if else statement)
+
+// next question is if they want any numeric characters (conditional statement)
+
+// do they want any lowercase characters (conditional statement)
+
+// do they want any uppercase characters (conditional statement)
